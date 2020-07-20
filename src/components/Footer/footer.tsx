8c9a9679e224aea988f189a6b1a206b925b79a4d
Akshat <33050725+aaaakshat@users.noreply.github.com>
2020-07-20 23:15:58 +0800

    Update footer styling and add credits

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -6,7 +6,19 @@ import "./footer.scss"
 
 const Footer = props => (
   <footer className={`Footer ${props.className}`}>
-    <span className="Footer__legal">© {new Date().getFullYear()}</span>
+    <h2 className="Footer__title">{props.siteTitle}</h2>
+    <span className="Footer__legal">
+      © {new Date().getFullYear()} {props.siteAuthor}. All rights reserved.
+    </span>
+    {/* I'd appreciate it if you keep the credits :) */}
+    <a
+      href="https://github.com/aaaakshat/gatsby-starter-texblog"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="Footer__citation"
+    >
+      Using TeXBlog by Akshat Bisht
+    </a>
   </footer>
 )
 
