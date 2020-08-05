edf2af39de580f940e91ac35acf0d3ca92b3ffa8
Akshat <33050725+aaaakshat@users.noreply.github.com>
2020-08-05 17:05:01 +0800

    Add code prism code highlighting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,6 +6,11 @@
 
 import "katex/dist/katex.min.css"
 
+import React from "react"
+import { MDXProvider } from "@mdx-js/react"
+import Highlight, { defaultProps } from "prism-react-renderer"
+import github from "prism-react-renderer/themes/github"
+
 const loadExternalStyles = url => {
   const styles = document.createElement("link")
   styles.rel = "stylesheet"
@@ -22,3 +27,44 @@ export const onClientEntry = () => {
     )
   }
 }
+
+// Credits: https://github.com/kunalJa/gatsby-starter-math-blog
+const component = {
+  pre: props => {
+    const className = props.children.props.className || ""
+    const matches = className.match(/language-(?<lang>.*)/)
+    return (
+      <Highlight
+        {...defaultProps}
+        code={props.children.props.children}
+        language={
+          matches && matches.groups && matches.groups.lang
+            ? matches.groups.lang
+            : ""
+        }
+        theme={github}
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className + " overflow-auto"} style={style}>
+            {tokens.map((line, i) => {
+              if (i === tokens.length - 1) {
+                return null
+              }
+              return (
+                <div {...getLineProps({ line, key: i })}>
+                  {line.map((token, key) => {
+                    return <span {...getTokenProps({ token, key })} />
+                  })}
+                </div>
+              )
+            })}
+          </pre>
+        )}
+      </Highlight>
+    )
+  },
+}
+export const wrapRootElement = ({ element }) => {
+  return <MDXProvider components={component}>{element}</MDXProvider>
+}
+
